b6f8de09bb9bfe56264f994b16a946d197972130
james-mbuvi <james.mbuvi@student.moringaschool.com>
2024-03-07 15:39:53 +0300

    completed Assignment

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,44 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Only instances of Pet can be added as pets")
+        self._pets.append(pet)
+        pet.set_owner(self)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.pet_type = pet_type
+        self.owner = owner
+        self.__class__.all.append(self)
+
+    def set_owner(self, owner):
+        if not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of Owner class")
+        self.owner = owner
+
+
+
+
+
+
+
+
+
+
